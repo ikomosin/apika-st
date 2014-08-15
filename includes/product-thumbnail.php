af0459be877fec25780b0c4142090598bf250ddd
Ivan <Bergerivan@mail.ru>
2014-08-16 01:18:31 +0300

    all pages

--- includes/product-thumbnail.php
+++ includes/product-thumbnail.php
@@ -1,19 +1,3 @@
 <div class="product-thumb">
 	<div class="product-thumb-text">NO IMAGE</div>
 </div>
-<style type="text/css">
-	.product-thumb{
-		height: 200px;
-		border: solid 5px #ccc;
-		background: #F3F3F3;
-		text-align: center;
-		position: relative;
-	}
-	.product-thumb-text{
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		margin-top: -10px;
-		margin-left: -30px;
-	}
-</style>
